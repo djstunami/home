37d225cf7c7bad83769803f4ef89ed8ab1462794
djstunami <djstunami@users.noreply.github.com>
2024-04-02 08:47:19 -0600

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,7 @@ Invoke-Expression (&starship init powershell)
 remove-item Alias:ls
 function ll { lsd -al $args $_ }
 function ls { lsd $args $_ }
+
+# If in corp env. file for your user/host will be in One Drive, e.g.
+# C:\Users\me\OneDrive-directory\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# Otherwise, just make a \WindowsPowerShell\Microsoft.PowerShell_profile.ps1 directory/file in your Documents directory

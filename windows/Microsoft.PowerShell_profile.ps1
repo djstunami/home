ed37345bcdec6140a05d6d4903ca0206f632fe20
djstunami <djstunami@users.noreply.github.com>
2024-04-02 08:42:18 -0600

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
 Invoke-Expression (&starship init powershell)
+# Kept for reference, don't use Aliases, use functions
 # Creating Aliases https://stackoverflow.com/questions/4166370/how-can-i-write-a-powershell-alias-with-arguments-in-the-middle
-filter ll { lsd "-al $args $_" }
-filter ls { lsd "$args $_" }
+remove-item Alias:ls
+function ll { lsd -al $args $_ }
+function ls { lsd $args $_ }
